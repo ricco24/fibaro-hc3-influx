0a711682ed6575e0daa2a3bae8103355547b695d
Samuel kelemen <kelemen.samuel@gmail.com>
2020-04-18 22:56:16 +0200

    Update logRefreshStatesComand

--- src/Command/LogRefreshStatesCommand.php
+++ src/Command/LogRefreshStatesCommand.php
@@ -3,6 +3,7 @@
 namespace F3ILog\Command;
 
 use F3ILog\HCClient\HCClient;
+use F3ILog\Storage\Storage;
 use GuzzleHttp\Client;
 use InfluxDB\Database;
 use InfluxDB\Point;
@@ -18,18 +19,18 @@ class LogRefreshStatesCommand extends Command
 
     private $hcClient;
 
-    private $lastFile;
+    private $storage;
 
     private $guzzle;
 
     private $io;
 
-    public function __construct(Database $influxDb, HCClient $hcClient, $lastFile)
+    public function __construct(Database $influxDb, HCClient $hcClient, Storage $storage)
     {
         parent::__construct();
         $this->influxDb = $influxDb;
         $this->hcClient = $hcClient;
-        $this->lastFile = $lastFile;
+        $this->storage = $storage;
         $this->guzzle = new Client();
     }
 
@@ -49,37 +50,78 @@ class LogRefreshStatesCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): ?int
     {
+        $startTime = microtime(true);
         $this->io = new SymfonyStyle($input, $output);
+        $this->io->title('Events import from HC3 to InfluxDB');
+
+
+        $this->io->section('Data preparation');
+
+        $this->io->write(' * loading devices (API) ... ');
         $devices = $this->hcClient->devices();
-        $this->io->writeln('-> devices loaded');
+        if ($devices === null) {
+            $this->io->writeln('<error>error</error>');
+            return 1;
+        }
+        $this->io->writeln(sprintf('<info>%d</info> devices loaded', count($devices)));
+
+        $this->io->write(' * loading rooms (API) ... ');
         $rooms = $this->hcClient->rooms();
-        $this->io->writeln('-> rooms loaded');
+        if ($rooms === null) {
+            $this->io->writeln('<error>error</error>');
+            return 1;
+        }
+        $this->io->writeln(sprintf('<info>%d</info> rooms loaded', count($rooms)));
 
-        $lastFile = @file_get_contents($this->lastFile);
-        $last = $lastFile ? (int) $lastFile : 1;
+        $this->io->write(' * loading sections (API) ... ');
+        $sections = $this->hcClient->sections();
+        if ($sections === null) {
+            $this->io->writeln('<error>error</error>');
+            return 1;
+        }
+        $this->io->writeln(sprintf('<info>%d</info> sections loaded', count($sections)));
 
-        $this->io->writeln(sprintf('-> loading refreshStates (poll) for last %d', $last));
+        $this->io->write(' * loading saved last ... ');
+        $last = $this->storage->load('refreshStates');
+        if ($last) {
+            $this->io->writeln("<info>$last</info>");
+        } else {
+            $this->io->writeln("No saved last");
+            $last = 1;
+        }
+
+        $this->io->section('Import');
+
+        $this->io->writeln(sprintf(' * loading refreshStates for last <info>%d</info>', $last));
         $poll = $this->hcClient->refreshStates($last);
         if (!isset($poll['changes'])) {
-            throw new Exception('No changes key in refreshStates response');
+            $this->io->writeln('<error>No changes key in refreshStates response</error>');
+            return 1;
         }
-        $this->io->writeln('-> refreshStates (poll) loaded');
+        $this->io->writeln(' * refreshStates loaded');
 
         $timestamp = $poll['timestamp'];
         if (isset($poll['last'])) {
-            file_put_contents($this->lastFile, $poll['last']);
+            $this->storage->store('refreshStates', $poll['last']);
         }
 
+        $points = [];
         foreach ($poll['changes'] as $change) {
-            if (!isset($change['id']) || !isset($change['value'])) {
-                continue;
+            $point = $this->createInfluxPoint($change, $devices, $rooms, $sections, $timestamp);
+            if ($point) {
+                $points[] = $point;
             }
-            $id = $change['id'];
-            $device = $devices[$id];
-            $room = $this->getRoomFromDevice($device, $rooms);
-            $this->io->writeln(sprintf('-> saving <info>%s</info>', $device['name']));
-            $this->saveToInflux($id, $change, $device, $room, $timestamp);
         }
+
+        if (count($points)) {
+            $this->influxDb->writePoints($points, Database::PRECISION_SECONDS);
+        }
+
+        $this->io->section('Result');
+        $this->io->writeln(sprintf("Duration: <info>%.2fs</info>", microtime(true) - $startTime));
+        $this->io->writeln(sprintf("Total changes loaded: <info>%d</info>", count($poll['changes'])));
+        $this->io->writeln(sprintf("Total changes saved: <info>%d</info>", count($points)));
+        $this->io->newLine();
 
         return 0;
     }
@@ -108,8 +150,29 @@ class LogRefreshStatesCommand extends Command
         return $rooms[$device['roomID']];
     }
 
-    private function saveToInflux($id, $change, $device, $room, $timestamp)
+    private function getSectionFromRoom($room, $sections)
     {
+        if (!isset($room['sectionID'])) {
+            return null;
+        }
+
+        if (!isset($sections[$room['sectionID']])) {
+            return null;
+        }
+
+        return $sections[$room['sectionID']];
+    }
+
+    private function createInfluxPoint(array $change, array $devices, array $rooms, array $sections, $timestamp)
+    {
+        if (!isset($change['id'])) {
+            return null;
+        }
+
+        $device = $devices[$change['id']];
+        $room = $this->getRoomFromDevice($device, $rooms);
+        $section = $this->getSectionFromRoom($room, $sections);
+
         $fields = [];
         foreach ($change as $k => $v) {
             if (in_array($k, ['id', 'log', 'logTemp', 'lastBreached'])) {
@@ -118,19 +181,24 @@ class LogRefreshStatesCommand extends Command
             $fields[$k] = $this->getFieldValue($v);
         }
 
-        $points = [
-            new Point(
-                $device['type'],
-                null,
-                [
-                    'sensor_id' => $id,
-                    'sensor_name' => $device['name'],
-                    'room_name' => $room && isset($room['name']) ? $room['name'] : 'None',
-                    'room_id' => $room && isset($room['id']) ? $room['id'] : 'None',
-                ],
-                $fields,
-                (int) $timestamp)
-        ];
-        $this->influxDb->writePoints($points, Database::PRECISION_SECONDS);
+        if (count($fields) === 0) {
+            return null;
+        }
+
+        return new Point(
+            'refreshStates.' . $device['type'],
+            null,
+            [
+                'device_id' => $device['id'],
+                'device_name' => $device['name'],
+                'device_type' => $device['type'],
+                'room_name' => $room && isset($room['name']) ? $room['name'] : 'None',
+                'room_id' => $room && isset($room['id']) ? $room['id'] : 'None',
+                'section_name' => $section && isset($section['name']) ? $section['name'] : 'None',
+                'section_id' => $section && isset($section['id']) ? $section['id'] : 'None'
+            ],
+            $fields,
+            (int) $timestamp
+        );
     }
 }
